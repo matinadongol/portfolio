3ce8c7e62ca2d88b2afe6efb667783e2fb4e824e
matinadongol <dongolmt@gmail.com>
2024-07-12 05:18:47 -0400

    added experience section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './about/About';
+import Experience from './experience/Experience';
 import Home from './home/Home';
 import Skills from './skills/Skills';
 
@@ -9,6 +10,7 @@ function App() {
       <Home/>
       <About/>
       <Skills/>
+      <Experience/>
     </div>
   );
 }
---
+++ portfolio/src/experience/Experience.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "../experience/Experience.css"
+
+export default function Experience() {
+  return (
+    <>
+      <div className="experienceContainer">
+        <h1>Experience</h1>
+        <div className="timeline">
+        <div className="container left blackberry">
+            <div className="content">
+                <h3>Software Developer CO-OP</h3>
+                <h5>BlackBerry Limited <span>Jan 2024 - April 2024</span></h5>
+                <p>Designed and implemented software features and fixes in Objective-C as part of the cybersecurity R&D team. Contributed creative software ideas and actively participated in the software development cycle. Collaborated with a diverse team to ensure secure mobile app development aligned with industry best practices.</p>
+            </div>
+        </div>
+        <div className="container right techtronix">
+            <div className="content">
+                <h3>REACT Native Developer</h3>
+                <h5>Techtronix Nepal <span>Jan 2024 - April 2024</span></h5>
+                <p>Designed and implemented software features and fixes in Objective-C as part of the cybersecurity R&D team. Contributed creative software ideas and actively participated in the software development cycle. Collaborated with a diverse team to ensure secure mobile app development aligned with industry best practices.</p>
+            </div>
+        </div>
+        <div className="container left pathway">
+            <div className="content">
+                <h3>Full Stack Developer</h3>
+                <h5>Pathway Technologies <span>Jan 2024 - April 2024</span></h5>
+                <p>Designed and implemented software features and fixes in Objective-C as part of the cybersecurity R&D team. Contributed creative software ideas and actively participated in the software development cycle. Collaborated with a diverse team to ensure secure mobile app development aligned with industry best practices.</p>
+            </div>
+        </div>
+    </div>
+      </div>
+    </>
+  )
+}
+
+ 
