30fda8c2e619d77123f1daeef69a6b10e9ea46a0
matinadongol <dongolmt@gmail.com>
2024-07-13 04:26:17 -0400

    added projects section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import About from './about/About';
 import Education from './education/Education';
 import Experience from './experience/Experience';
 import Home from './home/Home';
+import Projects from './projects/Projects';
 import Skills from './skills/Skills';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <About/>
       <Skills/>
       <Experience/>
+      <Projects/>
       <Education/>
     </div>
   );
---
+++ portfolio/src/projects/Projects.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import '../projects/Projects.css'
+
+export default function Projects() {
+  return (
+    <>
+    <div className="projectsContainer">
+      <h1>My work</h1>
+      <div className="projectsMain">
+        <div className="projectsSub">
+          <a href="https://github.com/matinadongol/chattpatte">
+            <h5>restaurant website</h5>
+            <p>Built on the MERN stack, the e-commerce platform seamlessly integrates features like product browsing, user authentication, and effective order management.</p>
+          </a>
+        </div>
+        <div className="projectsSub">
+          <a href="https://github.com/matinadongol/TravelTracker">
+            <h5>Travel Tracker App</h5>
+            <p>Built on the MERN stack, the e-commerce platform seamlessly integrates features like product browsing, user authentication, and effective order management.</p>
+          </a>
+        </div>
+        <div className="projectsSub">
+          <a href="https://github.com/matinadongol/news_portal">
+            <h5>News Portal</h5>
+            <p>Built on the MERN stack, the e-commerce platform seamlessly integrates features like product browsing, user authentication, and effective order management.</p>
+          </a>
+        </div>
+        <div className="projectsSub">
+          <a href="https://github.com/matinadongol/machchapuchreForm">
+            <h5>Application Form</h5>
+            <p>Built on the MERN stack, the e-commerce platform seamlessly integrates features like product browsing, user authentication, and effective order management.</p>
+          </a>
+        </div>
+        <div className="projectsSub">
+          <a href="https://github.com/matinadongol/ecommerce">
+            <h5>Ecommerce Site</h5>
+            <p>Built on the MERN stack, the e-commerce platform seamlessly integrates features like product browsing, user authentication, and effective order management.</p>
+          </a>
+        </div>
+        <div className="projectsSub">
+          <a href="https://github.com/matinadongol/cowayhome">
+            <h5>Commercial Site</h5>
+            <p>Built on the MERN stack, the e-commerce platform seamlessly integrates features like product browsing, user authentication, and effective order management.</p>
+          </a>
+        </div>
+        <div className="projectsSub">
+          <a href="https://github.com/matinadongol/cowayhome">
+            <h5>Commercial Site</h5>
+            <p>Built on the MERN stack, the e-commerce platform seamlessly integrates features like product browsing, user authentication, and effective order management.</p>
+          </a>
+        </div>
+        <div className="projectsSub">
+          <a href="https://github.com/matinadongol/cowayhome">
+            <h5>Commercial Site</h5>
+            <p>Built on the MERN stack, the e-commerce platform seamlessly integrates features like product browsing, user authentication, and effective order management.</p>
+          </a>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
